175cfb5940fb9d01b5e9da032cf5082ce15779ea
dersaky <dimagagarin2010@gmail.com>
2025-07-05 20:25:18 +0300

    Fix: Corrected voting link in main menu

--- resources/js/Shared/MainMenu.vue
+++ resources/js/Shared/MainMenu.vue
@@ -11,8 +11,8 @@
       <Link class="flex items-center justify-center w-10 h-10 rounded-full hover:bg-gray-200/80 transition-colors duration-200" :title="$t('main_menu.home')" href="/">
         <icon name="house" class="w-5 h-5" :class="isUrl('') ? 'fill-green-600' : 'fill-gray-500 group-hover:fill-green-500'" />
       </Link>
-      <Link class="flex items-center justify-center w-10 h-10 rounded-full hover:bg-gray-200/80 transition-colors duration-200" :title="$t('main_menu.voting')" href="/voting">
-        <icon name="vote" class="w-5 h-5" :class="isUrl('voting') ? 'fill-green-600' : 'fill-gray-500 group-hover:fill-green-500'" />
+      <Link class="flex items-center justify-center w-10 h-10 rounded-full hover:bg-gray-200/80 transition-colors duration-200" :title="$t('main_menu.voting')" href="/votings">
+        <icon name="vote" class="w-5 h-5" :class="isUrl('votings') ? 'fill-green-600' : 'fill-gray-500 group-hover:fill-green-500'" />
       </Link>
       <Link class="flex items-center justify-center w-10 h-10 rounded-full hover:bg-gray-200/80 transition-colors duration-200" :title="$t('main_menu.petitions')" href="/petitions">
         <icon name="petition" class="w-5 h-5" :class="isUrl('petitions') ? 'fill-green-600' : 'fill-gray-500 group-hover:fill-green-500'" />
